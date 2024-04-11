fe83f15ce3a6700b7c9943949ad6fe69be2f5e12
Muhammet Umut Kaya <umutkaya199@gmail.com>
2024-04-11 21:02:09 +0300

    Create Main.java

---
+++ palindromeNumbers/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class Main {
+
+    // Palindrom Sayı Nedir ? Palindromik sayı, iki taraftan okunduğu zaman okunuş yönüyle aynı olan sayılardır.
+    // Örnek: 1, 4, 8, 99, 101, 363, 4004, 9889....
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(4));
+
+    }
+}
